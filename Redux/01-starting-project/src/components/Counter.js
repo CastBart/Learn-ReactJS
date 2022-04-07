bfdf90b315fe13ee8b9ea2bf91907439aa4e6a95
CastBart <C00205464@itcarlow.ie>
2022-04-07 12:30:55 +0100

    multiple states using redux toolkit

--- Redux/01-starting-project/src/components/Counter.js
+++ Redux/01-starting-project/src/components/Counter.js
@@ -1,29 +1,40 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/counter";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
+  const showCounter = useSelector((state) => state.counter.showCounter);
 
-  const incrementHandler = ()=>{
-    dispatch({
-      type: 'increment'
-    })
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
   };
-  const decrementHandler = ()=>{
-    dispatch({
-      type: 'decrement'
-    })
+
+
+  /**
+   * * when passing an action value. redux toolkit will 
+   * * create an object => {type: SOME_UNIQUE_IDENTIFIER, payload: 5} number 5 links to the value passed into the increase function
+   */
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
   };
-  const toggleCounterHandler = () => {};
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler} >Decrement</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- Redux/01-starting-project/src/store/index.js
+++ Redux/01-starting-project/src/store/index.js
@@ -1,19 +1,68 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth";
+import counterReducer from "./counter";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-  return state;
-};
 
-const store = createStore(counterReducer);
+
+
+/**
+ * *this call will return an action object of this shape:
+ * *{type: 'auto-generated unique identifier}
+ * ! counterSlicer.actions.toggleCounter();
+ */
+
+/**
+ * * Using cofigureStore instead of createStore as it is good practice
+ * * for bigger applications that have more reducer functions
+ */
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
+});
+
+// /**
+//  * * Reducer function which handles all action types
+//  * * This is original way of working with redux where
+//  * * the reducer funtion is bound to the store
+//  * @param {automaticly passed down to retrive previous state} state
+//  * @param { automaticly passed down to process a given action} action
+//  * @returns
+//  */
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.value,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "toggle") {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// /**
+//  * * Creating the store and binding the appropriate reducer function
+//  * * which takes care of all action types
+// ! const store = createStore(counterReducer);
+//  */
 
 export default store;
